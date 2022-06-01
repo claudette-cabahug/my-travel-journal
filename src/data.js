99a23d0234f810b0c728ba5abcb25cb06453e61f
Claudette Cabahug <claudette.cabahug@outlook.com>
2022-06-02 10:59:17 +1200

    add a data file for travel information

---
+++ src/data.js
@@ -0,0 +1,29 @@
+export default [
+  {
+    title: "SEA Aquarium",
+    location: "Singapore",
+    googleMapsUrl: "https://goo.gl/maps/nbmj2TMmj18AzrSb6",
+    startDate: "17 December 2016",
+    endDate: "18 December 2016",
+    description: "S.E.A. Aquarium is one of the world’s largest aquariums, home to more than 100,000 marine animals representing 1,000 species across 45 diverse habitats. The vast array of aquatic life is complemented by interactive programmes, up-close animal encounters and immersive learning journeys to inspire visitors to protect the world’s oceans.",
+    imageUrl: "https://res.klook.com/images/fl_lossy.progressive,q_65/c_fill,w_3000,h_2002,f_auto/w_80,x_15,y_15,g_south_west,l_Klook_water_br_trans_yhcmh3/activities/ccfgnagsrilolytkoegu/SEAAquarium%E2%84%A2One-DayTicket-Klook.jpg"
+  },
+  {
+    title: "Legoland",
+    location: "Malaysia",
+    googleMapsUrl: "https://goo.gl/maps/jwhGsfXJUCrWUd6h9",
+    startDate: "18 December 2016",
+    endDate: "19 December 2016",
+    description: "Home to the most successful rides, slides, shows and attractions from LEGOLAND Parks around the world, LEGOLAND Malaysia Resort is tailor-made for the local environment. It covers the same area of more than 50 full size football pitches and contains more than 15,000 LEGO models made from over 60 million LEGO bricks. Other LEGOLAND Resorts are located in Japan, United Arab Emirates, Denmark, Germany, United States, and the United Kingdom.",
+    imageUrl: "https://media.tacdn.com/media/attractions-splice-spp-674x446/06/71/27/4a.jpg"
+  },
+  {
+    title: "Tarawera Ultramarathon",
+    location: "New Zealand",
+    googleMapsUrl: "https://goo.gl/maps/SqZxazRgc1bgt8sq8",
+    startDate: "17 February 2017",
+    endDate: "18 February 2017",
+    description: "Tarawera Ultramarathon is a trail running event which includes a 21km, 50km, 102km and a 100-mile ultramarathon trail race, with the inaugural even held in 2009, it is part of the Ultra-Trail World Tour. It is held annually in February and takes place in Rotorua, New Zealand.",
+    imageUrl: "https://www.taraweraultra.co.nz/assets/Uploads/Gallery/_resampled/ScaleWidthWyI5MDAiXQ/runners-bridge-river-content.jpg"
+  }
+]
