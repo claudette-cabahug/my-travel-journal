50c912306e0e2317c50cc66333590b793a987ae0
Claudette Cabahug <claudette.cabahug@outlook.com>
2022-06-03 11:08:47 +1200

    add a span element and changed alt in img to be more meaningful

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,12 +5,16 @@ export default function Card(props) {
 
   return (
     <div className="card--container">
-      <img src={props.image} alt="card image" className="card--image" />
+      <img src={props.image} alt="place travelled" className="card--image" />
       <div>
         <section className="card--section-location">
           <img src={pin_icon} className="card--icon-pin"/>
           <h5 className="card--location">{props.location}</h5>
-          <a href={props.mapUrl} className="card--location-link">View on Google Maps</a>
+          <a href={props.mapUrl}>
+            <span className="card--location-link">
+            View on Google Maps
+            </span>
+          </a>
         </section>
         <h1 className="card--title">{props.title}</h1>
         <h5 className="card--period">{props.start} - {props.end}</h5>
